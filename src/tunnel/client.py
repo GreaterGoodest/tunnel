93f7d29a53d75b359cf41078d0e7a02d70c59b65
Ryan Good <usafaryangood@gmail.com>
2022-05-04 14:32:33 -0500

    master: implements tunneled traffic transfer Changes to be committed: 	modified:   client.py

--- src/tunnel/client.py
+++ src/tunnel/client.py
@@ -6,6 +6,9 @@ LOCAL_PORT = 1336
 REMOTE_ADDR = '127.0.0.1'
 REMOTE_PORT = 1337
 
+KB = 1024
+DATA_SIZE = 1 * KB
+
 def connect_to_server() -> socket.socket:
     """Connect to remote server for tunneling."""
     remote_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,7 +20,7 @@ def connect_to_server() -> socket.socket:
     print(f'Successfullly connect to remote server at {REMOTE_ADDR}:{REMOTE_PORT}.')
     return remote_conn
 
-def listen_local():
+def listen_local() -> socket.socket:
     """Listen for local data to tunnel."""
     local_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     local_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -31,18 +34,37 @@ def listen_local():
     print('Received local connection')
     return local_conn
 
-def tunnel_loop(remote_conn: socket.socket):
+def tunnel_loop(local_conn: socket.socket, remote_conn: socket.socket) -> None:
     """Main tunnel traffic exchange loop."""
+    data = None
+    while True:
+        try:
+            data = local_conn.recv(DATA_SIZE)
+        except BlockingIOError:
+            pass
+        if data:
+            remote_conn.send(data)
+        data = None
+        try:
+            data = remote_conn.recv(DATA_SIZE)
+        except BlockingIOError:
+            pass
+        if data:
+            local_conn.send(data)
+        data = None
 
 def init(dest: int):
     """Tunnel setup and initialization."""
     remote_conn = connect_to_server()
     if not remote_conn:
         return
+    remote_conn.setblocking(0)
+
     local_conn = listen_local()
     if not local_conn:
         return
-    tunnel_loop(remote_conn)
+    local_conn.setblocking(0)
+    tunnel_loop(local_conn, remote_conn)
 
 
 if __name__ == "__main__":
